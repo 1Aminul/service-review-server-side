2005c7baafbfb6358cb90885c9527fa48c745e2c
MD Aminul Islam <aminulsc12@gmail.com>
2022-11-08 20:22:46 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.flkzc5o.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const dentistCollection =  client.db('dentist').collection('service');
+
+async function run(){
+    try{
+        app.get('/services', async(req, res)=>{
+            const query = {}
+
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.log(err))
+
 
 
 app.get('/', (req, res)=>{
