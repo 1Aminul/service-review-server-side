daa9cae412d2caeb7075cab557987454e98cebe4
MD Aminul Islam <aminulsc12@gmail.com>
2022-11-08 22:38:01 +0600

    service added in server by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000
@@ -21,7 +21,17 @@ async function run(){
     try{
         app.get('/services', async(req, res)=>{
             const query = {}
+            const cursor =  dentistCollection.find(query)
+            const service = await cursor.toArray()
+            res.send(service)
 
+        })
+
+        app.get('/services/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await dentistCollection.findOne(query)
+            res.send(result)
         })
     }
     finally{
