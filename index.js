749dcd698a420f3d95bab64684bc8341def6ed93
MD Aminul Islam <aminulsc12@gmail.com>
2022-11-10 15:33:07 +0600

    put modify for update data

--- index.js
+++ index.js
@@ -71,6 +71,37 @@ async function run(){
             res.send(reviews) 
             
         })
+        //update date pertiary
+        app.put('/reviews/:id', async (req, res)=>{
+            const id = req.params.id
+            const user = req.body
+            const query = { _id: ObjectId (id) }
+            const updatedoc = {
+                $set: {
+                    review: user.review
+                }
+            }
+            const result = await reviewCollection.updateOne(query, updatedoc)
+            console.log(result);
+            // res.send(result)
+        })
+
+        //delete data
+        app.delete('/reviews/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const result = await reviewCollection.deleteOne(query)
+            res.send(result)
+
+        })
+
+        app.get('/reviews/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const result = await reviewCollection.findOne(query)
+            res.send(result) 
+        })
+
 
 
     }
